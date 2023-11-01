ce5e66a5cbcbe97e3e04132f71573afc58b9a3b6
Tshattuck1 <120345685+Tshattuck1@users.noreply.github.com>
2023-11-01 23:39:46 +0000

    adding initial work

---
+++ challengeSet1.kt
@@ -0,0 +1,5 @@
+fun hexToBase64(value: String){
+    value = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    var test = value
+    Base64.encodeToString(bytes, Base64.DEFAULT)
+}
