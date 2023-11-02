503f5d126de2f59509f396f695754840a3bcb188
Thad <tshattuck@atlassian.com>
2023-11-03 10:57:33 +1100

    better englishScore frequencies

--- src/main/kotlin/Set1.kt
+++ src/main/kotlin/Set1.kt
@@ -32,11 +32,19 @@ import kotlin.io.encoding.ExperimentalEncodingApi
     }
 
     fun englishScore(text: String): Pair<String, Double> {
-        val score = text.filter { it in ('A'..'z') || it == ' ' }
-            .groupingBy { it.lowercaseChar() }
-            .eachCount().toList()
-            .sumOf { it.second }
-        return Pair(text, score.toDouble().div(text.length.toDouble()))
+        val englishFrequencies = mapOf(
+            'a' to 8.167, 'b' to 1.492, 'c' to 2.782, 'd' to 4.253, 'e' to 12.702,
+            'f' to 2.228, 'g' to 2.015, 'h' to 6.094, 'i' to 6.966, 'j' to 0.153,
+            'k' to 0.772, 'l' to 4.025, 'm' to 2.406, 'n' to 6.749, 'o' to 7.507,
+            'p' to 1.929, 'q' to 0.095, 'r' to 5.987, 's' to 6.327, 't' to 9.056,
+            'u' to 2.758, 'v' to 0.978, 'w' to 2.360, 'x' to 0.150, 'y' to 1.974,
+            'z' to 0.074, ' ' to 13.000
+        )
+
+        val score = text.filter { it.lowercaseChar() in englishFrequencies.keys }
+            .sumOf { englishFrequencies[it.lowercaseChar()] ?: 0.0 }
+
+        return Pair(text, score / text.length)
     }
 
     fun xor(input: ByteArray, char: Char): String {
